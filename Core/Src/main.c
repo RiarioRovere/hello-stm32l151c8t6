76bcf3615103155c32d1d0dd258bb543258b16c0
rover <riariorovere@yandex.ru>
2022-06-03 02:25:26 +0300

    add USART1 with interruption

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,6 +41,8 @@
 /* Private variables ---------------------------------------------------------*/
  TIM_HandleTypeDef htim3;
 
+UART_HandleTypeDef huart1;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -49,6 +51,7 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM3_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -87,27 +90,49 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM3_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 
-    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_1);
-    TIM3->CCR2 = 252;
+//    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+//    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_1);
+
+    uint8_t str[] = "USART1 is working!\r\n";
+    uint8_t receive_str[15] = "";
 
   while (1)
   {
+      int limit = 240;
+      for (int i = 256; i >= limit; i--) {
+          TIM3->CCR2 = i;
+          TIM3->CCR3 = i;
+          TIM3->CCR4 = i;
+          HAL_Delay(50);
+      }
+      for (int i = limit; i <= 256; i++) {
+          TIM3->CCR2 = i;
+          TIM3->CCR3 = i;
+          TIM3->CCR4 = i;
+          HAL_Delay(50);
+      }
+      HAL_UART_Receive_IT(&huart1, receive_str, 15);
+      HAL_UART_Transmit(&huart1, receive_str, 15, 0xFFFF);
+      HAL_UART_Transmit(&huart1, "bip\r\n", 5, 0xFFFF);
+      receive_str[0] = '\0';
+
 
 //      HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
 //      HAL_Delay(1000);
 
     /* USER CODE END WHILE */
-
 
     /* USER CODE BEGIN 3 */
   }
@@ -209,9 +234,51 @@ static void MX_TIM3_Init(void)
   {
     Error_Handler();
   }
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
   /* USER CODE BEGIN TIM3_Init 2 */
 
   /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
 
 }
 
@@ -229,10 +296,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_12|GPIO_PIN_15, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12|GPIO_PIN_15, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PB0 PB1 PB12 PB15 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_12|GPIO_PIN_15;
+  /*Configure GPIO pins : PB12 PB15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
